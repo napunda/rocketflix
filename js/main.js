bf885ed7b55cb254b245720dcc56509a036b72d4
napund4 <napunda@mail.com>
2022-11-12 20:42:18 -0400

    add tags

--- js/main.js
+++ js/main.js
@@ -2,23 +2,93 @@ import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
 
 const search = {
   genUrl() {
-    const category = ["popular", "upcoming", "top_rated", "now_playing"];
-    const ramdonCategory = Math.floor(Math.random() * 3);
-
+    let category = [];
     let randomPage = 1;
-    switch (ramdonCategory) {
-      case 0:
-        randomPage = Math.floor(Math.random() * 500) + 1;
-        break;
-      case 1:
-        randomPage = Math.floor(Math.random() * 20) + 1;
-        break;
-      case 2:
-        randomPage = Math.floor(Math.random() * 500) + 1;
-        break;
-      case 3:
-        randomPage = Math.floor(Math.random() * 90) + 1;
-        break;
+    let ramdonCategory = null;
+
+    if (category.length > 0) {
+      category = DOM.getTags();
+
+      if (category.length == 4) {
+        ramdonCategory = Math.floor(Math.random() * 3);
+        switch (ramdonCategory) {
+          case 0:
+            randomPage = Math.floor(Math.random() * 500) + 1;
+            break;
+          case 1:
+            randomPage = Math.floor(Math.random() * 20) + 1;
+            break;
+          case 2:
+            randomPage = Math.floor(Math.random() * 500) + 1;
+            break;
+          case 3:
+            randomPage = Math.floor(Math.random() * 90) + 1;
+            break;
+        }
+      }
+      if (category.length == 3) {
+        ramdonCategory = Math.floor(Math.random() * 2);
+
+        category.forEach((element) => {
+          if (element == "popular") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "upcoming") {
+            randomPage = Math.floor(Math.random() * 20) + 1;
+          } else if (element == "top_rated") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "now_playing") {
+            randomPage = Math.floor(Math.random() * 90) + 1;
+          }
+        });
+      }
+      if (category.length == 2) {
+        ramdonCategory = Math.floor(Math.random() * 1);
+
+        category.forEach((element) => {
+          if (element == "popular") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "upcoming") {
+            randomPage = Math.floor(Math.random() * 20) + 1;
+          } else if (element == "top_rated") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "now_playing") {
+            randomPage = Math.floor(Math.random() * 90) + 1;
+          }
+        });
+      }
+      if (category.length == 1) {
+        ramdonCategory = 0;
+
+        category.forEach((element) => {
+          if (element == "popular") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "upcoming") {
+            randomPage = Math.floor(Math.random() * 20) + 1;
+          } else if (element == "top_rated") {
+            randomPage = Math.floor(Math.random() * 500) + 1;
+          } else if (element == "now_playing") {
+            randomPage = Math.floor(Math.random() * 90) + 1;
+          }
+        });
+      }
+    } else {
+      category = ["popular", "upcoming", "top_rated", "now_playing"];
+      ramdonCategory = Math.floor(Math.random() * 3);
+
+      switch (ramdonCategory) {
+        case 0:
+          randomPage = Math.floor(Math.random() * 500) + 1;
+          break;
+        case 1:
+          randomPage = Math.floor(Math.random() * 20) + 1;
+          break;
+        case 2:
+          randomPage = Math.floor(Math.random() * 500) + 1;
+          break;
+        case 3:
+          randomPage = Math.floor(Math.random() * 90) + 1;
+          break;
+      }
     }
 
     const url = `${BASE_URL}${category[ramdonCategory]}?api_key=${API_KEY}&${language}&page=${randomPage}`;
@@ -62,11 +132,34 @@ const search = {
         img_path: moviesArrayFiltered[randomIndex].img_path,
       };
     } else {
-      newSearch();
+      app.newSearch();
     }
     return movie;
   },
-  insertHTML(movie) {
+};
+
+const DOM = {
+  getTags() {
+    const popular = document.querySelector("#popular");
+    const upcoming = document.querySelector("#upcoming");
+    const top_rated = document.querySelector("#top_rated");
+    const now_playing = document.querySelector("#now_playing");
+    let tags = [];
+    if (popular.checked == true) {
+      tags.push("popular");
+    }
+    if (upcoming.checked == true) {
+      tags.push("upcoming");
+    }
+    if (top_rated.checked == true) {
+      tags.push("top_rated");
+    }
+    if (now_playing.checked == true) {
+      tags.push("now_playing");
+    }
+    return tags;
+  },
+  insertMovie(movie) {
     const { title, year, overview, img_path } = movie;
 
     const html = `
@@ -82,18 +175,36 @@ const search = {
     let containerMovie = document.querySelector(".movie");
     containerMovie.innerHTML = html;
   },
+  removeMovie() {
+    let containerMovie = document.querySelector(".movie");
+    containerMovie.innerHTML = "";
+  },
+  addLoader() {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader-ative");
+  },
+  removeLoader() {
+    const loader = document.querySelector(".loader");
+    loader.classList.remove("loader-ative");
+  },
 };
 
-function init() {
-  const button = document.querySelector("#finder-movie-trigger");
-  button.addEventListener("click", newSearch);
-}
+// Application
 
-async function newSearch() {
-  const movies = await search.fetchMovies();
-  if (movies) {
-    search.insertHTML(search.getMovie(movies));
-  }
-}
+const app = {
+  init() {
+    const button = document.querySelector("#finder-movie-trigger");
+    button.addEventListener("click", app.newSearch);
+  },
+  async newSearch() {
+    DOM.removeMovie();
+    DOM.addLoader();
+    const movies = await search.fetchMovies();
+    if (movies) {
+      DOM.insertMovie(search.getMovie(movies));
+      DOM.removeLoader();
+    }
+  },
+};
 
-init();
+app.init();
